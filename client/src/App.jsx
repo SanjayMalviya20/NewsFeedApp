9d866bc1736dbccf7e47a619c32f246037cb2b42
SanjayMalviya <161587355+SanjayMalviya20@users.noreply.github.com>
2025-02-25 16:54:57 +0530

    new changes

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+import NewsFeed from "./components/NewsFeed";
+
+function App() {
+  return (
+    <div className="App">
+      <NewsFeed />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/redux/newSlice.js
@@ -0,0 +1,45 @@
+// src/redux/newsSlice.js
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Async thunk to fetch news from the backend API
+export const fetchNews = createAsyncThunk("news/fetchNews", async () => {
+  const response = await axios.get("https://news-feed-app-cw9n.vercel.app/api/news");
+  return response.data;
+});
+
+const newsSlice = createSlice({
+  name: "news",
+  initialState: {
+    newsList: [],
+    categorys: "All",
+    status: "idle",
+    error: null,
+  },
+  reducers: {
+    // Action to add a single news item (from Socket.io updates)
+    addNews: (state, action) => {
+      state.newsList.unshift(action.payload);
+    },
+    setCategorys: (state, action) => {
+      state.categorys =action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchNews.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchNews.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.newsList = action.payload;
+      })
+      .addCase(fetchNews.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { addNews ,setCategorys} = newsSlice.actions;
+export default newsSlice.reducer;
