4fca683042b824684c87a48f93b1624ad5af5f5c
SanjayMalviya <161587355+SanjayMalviya20@users.noreply.github.com>
2025-02-25 16:52:43 +0530

    Delete client directory

--- client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-// src/redux/store.js
-import { configureStore } from "@reduxjs/toolkit";
-import newsReducer from "./newSlice.js";
-
-export const store = configureStore({
-  reducer: {
-    news: newsReducer,
-  },
-});
