cb3ff8e1868505d307642113482dd5bebbbc6b12
SanjayMalviya <161587355+SanjayMalviya20@users.noreply.github.com>
2025-02-25 19:09:41 +0530

    Delete server directory

--- server/config/dbConnect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-const dbConnect = () => {
-    mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected")) .catch((error) => console.error("MongoDB connection error:", error));
-
-}
-
-export default dbConnect
